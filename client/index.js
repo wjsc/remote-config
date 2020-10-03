6cf8a98846cbc8ebd523922af7be71d7af93d5ad
Walter Scarano <walter.scarano@naranjax.com>
2020-10-03 17:35:31 -0300

    more changes

--- client/index.js
+++ client/index.js
@@ -2,5 +2,6 @@ const security = require('./security');
 const client = require('./client');
 
 module.exports = {
-    init: client.init(security, '../keys.proto')
+    init: client.init(security, '../keys.proto'),
+    security
 }

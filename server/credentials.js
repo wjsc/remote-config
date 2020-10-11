00f7e2093b059d05c4f11b8338920f7ab50ebd21
wsjc <walterscarano@outlook.com>
2020-10-11 17:33:13 -0300

    variable renamed

--- server/credentials.js
+++ server/credentials.js
@@ -1,24 +1,24 @@
 const assert = require('assert');
 
-const createServerCredentials = (grpc, caCertificate, privateKey, clientCertificate) => {
-    if(!caCertificate && !privateKey && !clientCertificate){
+const createServerCredentials = (grpc, caCertificate, privateKey, serverCertificate) => {
+    if(!caCertificate && !privateKey && !serverCertificate){
         console.warn('Server running without SSL/TLS Authentication');
         return createInsecureServerCredentials(grpc);
     }
     assert(caCertificate, 'SSL/TLS Authentication: CA certificate is required');
     assert(privateKey, 'SSL/TLS Authentication: Server private key is required');
-    assert(clientCertificate, 'SSL/TLS Authentication: Server certificate is required' );
-    return createSslServerCredentials(grpc, caCertificate, privateKey, clientCertificate, true);
+    assert(serverCertificate, 'SSL/TLS Authentication: Server certificate is required' );
+    return createSslServerCredentials(grpc, caCertificate, privateKey, serverCertificate, true);
 }
 
-const createSslServerCredentials = ( grpc, caCertificate, privateKey, clientCertificate, checkClientCertificate = true ) => 
+const createSslServerCredentials = ( grpc, caCertificate, privateKey, serverCertificate, checkserverCertificate = true ) => 
     grpc.ServerCredentials.createSsl( 
         Buffer.from(caCertificate, 'utf8'),
         [{
             private_key: Buffer.from(privateKey, 'utf8'),
-            cert_chain: Buffer.from(clientCertificate, 'utf8') 
+            cert_chain: Buffer.from(serverCertificate, 'utf8') 
         }],
-        checkClientCertificate
+        checkserverCertificate
         
     );
 

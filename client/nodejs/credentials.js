e7da0a2e4e2d2b0621d8fbd51840b9addc7afc1e
wsjc <walterscarano@outlook.com>
2020-10-13 18:34:12 -0300

    client certificate fix

--- client/nodejs/credentials.js
+++ client/nodejs/credentials.js
@@ -1,21 +1,32 @@
 const assert = require('assert');
 
 const createClientCredentials = (grpc, privateKey, caCertificate, clientCertificate) => {
-    if(!caCertificate && !privateKey && !clientCertificate){
+    if(!caCertificate){
         console.warn('Client connecting without SSL/TLS Authentication');
         return createInsecureClientCredentials(grpc);
+    }
+    if(!clientCertificate){
+        console.warn('Client connecting without Mutual SSL/TLS');
+        return createSslServerCredentials(grpc, caCertificate);
     }
     assert(caCertificate, 'SSL/TLS Authentication: CA certificate is required');
     assert(privateKey, 'SSL/TLS Authentication: Client private key is required');
     assert(clientCertificate, 'SSL/TLS Authentication: Client certificate is required' );
-    return createSslClientCredentials(grpc, caCertificate, privateKey, clientCertificate, true);
+    return createSslMutualCredentials(grpc, caCertificate, privateKey, clientCertificate);
 }
 
-const createSslClientCredentials = ( grpc, caCertificate, privateKey, clientCertificate ) => 
+const createSslMutualCredentials = ( grpc, caCertificate, privateKey, clientCertificate ) => 
     grpc.credentials.createSsl(
         Buffer.from(caCertificate, 'utf8'),
-        Buffer.from(privateKey, 'utf8'),
-        Buffer.from(clientCertificate, 'utf8')
+        privateKey && Buffer.from(privateKey, 'utf8'),
+        clientCertificate && Buffer.from(clientCertificate, 'utf8')
+    );
+
+const createSslServerCredentials = ( grpc, caCertificate, ) => 
+    grpc.credentials.createSsl(
+        Buffer.from(caCertificate, 'utf8'),
+        privateKey && Buffer.from(privateKey, 'utf8'),
+        clientCertificate && Buffer.from(clientCertificate, 'utf8')
     );
 
 const createInsecureClientCredentials = grpc => grpc.credentials.createInsecure();
